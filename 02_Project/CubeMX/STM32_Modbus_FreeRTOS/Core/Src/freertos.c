b700adccb8f430499e7e453f22734ef958c9de6d
ccw-w <a001dfj9@gmail.com>
2026-04-22 13:26:30 +0800

    refactor: split defaultTask into display ctrl data and comm tasks

--- 02_Project/CubeMX/STM32_Modbus_FreeRTOS/Core/Src/freertos.c
+++ 02_Project/CubeMX/STM32_Modbus_FreeRTOS/Core/Src/freertos.c
@@ -1,27 +1,28 @@
 /* USER CODE BEGIN Header */
 /**
-  ******************************************************************************
-  * File Name          : freertos.c
-  * Description        : Code for freertos applications
-  ******************************************************************************
-  * @attention
-  *
-  * Copyright (c) 2026 STMicroelectronics.
-  * All rights reserved.
-  *
-  * This software is licensed under terms that can be found in the LICENSE file
-  * in the root directory of this software component.
-  * If no LICENSE file comes with this software, it is provided AS-IS.
-  *
-  ******************************************************************************
-  */
+ ******************************************************************************
+ * File Name          : freertos.c
+ * Description        : Code for freertos applications
+ ******************************************************************************
+ * @attention
+ *
+ * Copyright (c) 2026 STMicroelectronics.
+ * All rights reserved.
+ *
+ * This software is licensed under terms that can be found in the LICENSE file
+ * in the root directory of this software component.
+ * If no LICENSE file comes with this software, it is provided AS-IS.
+ *
+ ******************************************************************************
+ */
 /* USER CODE END Header */
 
 /* Includes ------------------------------------------------------------------*/
 #include "FreeRTOS.h"
-#include "task.h"
-#include "main.h"
 #include "cmsis_os.h"
+#include "main.h"
+#include "task.h"
+
 
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
@@ -50,19 +51,33 @@
 /* USER CODE BEGIN Variables */
 
 /* USER CODE END Variables */
-/* Definitions for defaultTask */
-osThreadId_t defaultTaskHandle;
-const osThreadAttr_t defaultTask_attributes = {
-  .name = "defaultTask",
-  .stack_size = 128 * 4,
-  .priority = (osPriority_t) osPriorityNormal,
+/* Definitions for commTask */
+osThreadId_t commTaskHandle;
+const osThreadAttr_t commTask_attributes = {
+    .name = "commTask",
+    .stack_size = 128 * 4,
+    .priority = (osPriority_t)osPriorityNormal1,
 };
 /* Definitions for dataTask */
 osThreadId_t dataTaskHandle;
 const osThreadAttr_t dataTask_attributes = {
-  .name = "dataTask",
-  .stack_size = 256 * 4,
-  .priority = (osPriority_t) osPriorityNormal1,
+    .name = "dataTask",
+    .stack_size = 128 * 4,
+    .priority = (osPriority_t)osPriorityNormal1,
+};
+/* Definitions for displayTask */
+osThreadId_t displayTaskHandle;
+const osThreadAttr_t displayTask_attributes = {
+    .name = "displayTask",
+    .stack_size = 128 * 4,
+    .priority = (osPriority_t)osPriorityLow,
+};
+/* Definitions for ctrlTask */
+osThreadId_t ctrlTaskHandle;
+const osThreadAttr_t ctrlTask_attributes = {
+    .name = "ctrlTask",
+    .stack_size = 128 * 4,
+    .priority = (osPriority_t)osPriorityBelowNormal1,
 };
 
 /* Private function prototypes -----------------------------------------------*/
@@ -83,16 +98,18 @@ extern uint16_t g_last_temp_alarm_high;
 extern uint16_t g_last_humi_alarm_high;
 /* USER CODE END FunctionPrototypes */
 
-void StartDefaultTask(void *argument);
+void StartCommTask(void *argument);
 void StartDataTask(void *argument);
+void StartDisplayTask(void *argument);
+void StartCtrlTask(void *argument);
 
 void MX_FREERTOS_Init(void); /* (MISRA C 2004 rule 8.1) */
 
 /**
-  * @brief  FreeRTOS initialization
-  * @param  None
-  * @retval None
-  */
+ * @brief  FreeRTOS initialization
+ * @param  None
+ * @retval None
+ */
 void MX_FREERTOS_Init(void) {
   /* USER CODE BEGIN Init */
 
@@ -115,11 +132,18 @@ void MX_FREERTOS_Init(void) {
   /* USER CODE END RTOS_QUEUES */
 
   /* Create the thread(s) */
-  /* creation of defaultTask */
-  defaultTaskHandle = osThreadNew(StartDefaultTask, NULL, &defaultTask_attributes);
+  /* creation of commTask */
+  commTaskHandle = osThreadNew(StartCommTask, NULL, &commTask_attributes);
 
   /* creation of dataTask */
   dataTaskHandle = osThreadNew(StartDataTask, NULL, &dataTask_attributes);
+
+  /* creation of displayTask */
+  displayTaskHandle =
+      osThreadNew(StartDisplayTask, NULL, &displayTask_attributes);
+
+  /* creation of ctrlTask */
+  ctrlTaskHandle = osThreadNew(StartCtrlTask, NULL, &ctrlTask_attributes);
 
   /* USER CODE BEGIN RTOS_THREADS */
   /* add threads, ... */
@@ -128,78 +152,51 @@ void MX_FREERTOS_Init(void) {
   /* USER CODE BEGIN RTOS_EVENTS */
   /* add events, ... */
   /* USER CODE END RTOS_EVENTS */
-
 }
 
-/* USER CODE BEGIN Header_StartDefaultTask */
+/* USER CODE BEGIN Header_StartCommTask */
 /**
-  * @brief  Function implementing the defaultTask thread.
-  * @param  argument: Not used
-  * @retval None
-  */
-/* USER CODE END Header_StartDefaultTask */
-void StartDefaultTask(void *argument)
-{
-  /* USER CODE BEGIN StartDefaultTask */
+ * @brief  Function implementing the commTask thread.
+ * @param  argument: Not used
+ * @retval None
+ */
+/* USER CODE END Header_StartCommTask */
+void StartCommTask(void *argument) {
+  /* USER CODE BEGIN StartCommTask */
   /* Infinite loop */
-    for (;;) {
-      /* 每次循环都更新输出，LED闪烁 */
-      Device_Control_Update();
+  for (;;) {
+    if (Modbus_Frame_Flag) {
+      Modbus_Frame_Flag = 0;
 
-      if (HAL_GetTick() - g_last_oled_tick >= 500) {
-        g_last_oled_tick = HAL_GetTick();
-        OLED_Display_Update();
-      }
+      if (Modbus_Server() == 1)
+        Modbus_Register[REG_COMM_STATE] = 0;
+      else
+        Modbus_Register[REG_COMM_STATE] = 1;
 
-      if (Modbus_Register[REG_SLAVE_ADDR] != g_last_slave_addr ||
-          Modbus_Register[REG_TEMP_ALARM_HIGH] != g_last_temp_alarm_high ||
-          Modbus_Register[REG_HUMI_ALARM_HIGH] != g_last_humi_alarm_high) {
-        g_last_slave_addr = Modbus_Register[REG_SLAVE_ADDR];
-        g_last_temp_alarm_high = Modbus_Register[REG_TEMP_ALARM_HIGH];
-        g_last_humi_alarm_high = Modbus_Register[REG_HUMI_ALARM_HIGH];
+      memset(Modbus_RX_Buffer, 0, sizeof(Modbus_RX_Buffer));
+      Modbus_RX_Length = 0;
 
-        Param_RequestSave();
-      }
-
-      if (g_param_need_save && (HAL_GetTick() - g_param_save_tick >= 2000)) {
-        g_param_need_save = 0;
-        Param_SaveFromRegister();
-      }
-
-      if (Modbus_Frame_Flag) {
-        Modbus_Frame_Flag = 0;
-
-        if (Modbus_Server() == 1)
-          Modbus_Register[REG_COMM_STATE] = 0;
-        else
-          Modbus_Register[REG_COMM_STATE] = 1;
-
-        memset(Modbus_RX_Buffer, 0, sizeof(Modbus_RX_Buffer));
-        Modbus_RX_Length = 0;
-
-        HAL_UARTEx_ReceiveToIdle_DMA(&huart1, Modbus_RX_Buffer,
-                                     sizeof(Modbus_RX_Buffer));
-        __HAL_DMA_DISABLE_IT(huart1.hdmarx, DMA_IT_HT);
-      }
-
-      osDelay(10);
+      HAL_UARTEx_ReceiveToIdle_DMA(&huart1, Modbus_RX_Buffer,
+                                   sizeof(Modbus_RX_Buffer));
+      __HAL_DMA_DISABLE_IT(huart1.hdmarx, DMA_IT_HT);
     }
-  /* USER CODE END StartDefaultTask */
+
+    osDelay(10);
+  }
+  /* USER CODE END StartCommTask */
 }
 
 /* USER CODE BEGIN Header_StartDataTask */
 /**
-* @brief Function implementing the dataTask thread.
-* @param argument: Not used
-* @retval None
-*/
+ * @brief Function implementing the dataTask thread.
+ * @param argument: Not used
+ * @retval None
+ */
 /* USER CODE END Header_StartDataTask */
-void StartDataTask(void *argument)
-{
+void StartDataTask(void *argument) {
   /* USER CODE BEGIN StartDataTask */
   /* Infinite loop */
-  for(;;)
-  {
+  for (;;) {
     Device_Data_Update();
     Device_Alarm_Update();
     osDelay(1000);
@@ -207,8 +204,57 @@ void StartDataTask(void *argument)
   /* USER CODE END StartDataTask */
 }
 
+/* USER CODE BEGIN Header_StartDisplayTask */
+/**
+ * @brief Function implementing the displayTask thread.
+ * @param argument: Not used
+ * @retval None
+ */
+/* USER CODE END Header_StartDisplayTask */
+void StartDisplayTask(void *argument) {
+  /* USER CODE BEGIN StartDisplayTask */
+  /* Infinite loop */
+  for (;;) {
+    OLED_Display_Update();
+    osDelay(500);
+  }
+  /* USER CODE END StartDisplayTask */
+}
+
+/* USER CODE BEGIN Header_StartCtrlTask */
+/**
+ * @brief Function implementing the ctrlTask thread.
+ * @param argument: Not used
+ * @retval None
+ */
+/* USER CODE END Header_StartCtrlTask */
+void StartCtrlTask(void *argument) {
+  /* USER CODE BEGIN StartCtrlTask */
+  /* Infinite loop */
+  for (;;) {
+    Device_Control_Update();
+
+    if (Modbus_Register[REG_SLAVE_ADDR] != g_last_slave_addr ||
+        Modbus_Register[REG_TEMP_ALARM_HIGH] != g_last_temp_alarm_high ||
+        Modbus_Register[REG_HUMI_ALARM_HIGH] != g_last_humi_alarm_high) {
+      g_last_slave_addr = Modbus_Register[REG_SLAVE_ADDR];
+      g_last_temp_alarm_high = Modbus_Register[REG_TEMP_ALARM_HIGH];
+      g_last_humi_alarm_high = Modbus_Register[REG_HUMI_ALARM_HIGH];
+
+      Param_RequestSave();
+    }
+
+    if (g_param_need_save && (HAL_GetTick() - g_param_save_tick >= 2000)) {
+      g_param_need_save = 0;
+      Param_SaveFromRegister();
+    }
+
+    osDelay(100);
+  }
+  /* USER CODE END StartCtrlTask */
+}
+
 /* Private application code --------------------------------------------------*/
 /* USER CODE BEGIN Application */
 
 /* USER CODE END Application */
-
